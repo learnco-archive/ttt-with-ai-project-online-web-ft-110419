9a38f5d8d9a5b32302247d2b723fa0e6ab42867b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-30 21:28:56 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,44 +1,43 @@
 class Board
   attr_accessor :cells
 
-  def initialize
-    self.reset!
+  def initialize()
+    reset!
   end
 
   def reset!
-    self.cells = Array.new(9, " ")
+    @cells = Array.new(9, " ")
   end
 
   def display
-    binding.pry
-    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
     puts "-----------"
-    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
     puts "-----------"
-    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
-  end
-
-  def position(user_input)
-    self.cells[user_input.to_i - 1]
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
   def full?
-    self.cells.include?(" ") ? false : true
+    cells.all?{|token| token == "X" || token == "O"}
   end
 
   def turn_count
-    self.cells.select { |cell| cell != " "}.length
+    cells.count{|token| token == "X" || token == "O"}
   end
 
-  def taken?(index)
-    self.cells[index.to_i - 1] == "X" || self.cells[index.to_i - 1] == "O" ? true : false
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
   end
 
-  def valid_move?(user_input)
-    user_input.to_i.between?(1, 9) && !self.taken?(user_input)
+  def update(input, player)
+    cells[input.to_i-1] = player.token
   end
 
-  def update
-    # updates the cells in the board with the player token according to the input (FAILED - 1)
+  def position(input)
+    cells[input.to_i-1]
+  end
+
+  def taken?(input)
+    !(position(input) == " " || position(input) == "")
   end
 end
